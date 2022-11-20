97b60309bdd9c8e4d695e07c4dc00f1e855cf205
Lukasz Juraszek <juraszeklukasz@gmail.com>
2022-11-20 14:49:30 -0800

    Saving WIP, BUFFER_SIZE, Design

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
-build docker image:
-docker build -t cribl-api .
+# Description
+The Cribl-API service provides on-demand monitoring of various unix-based servers without having to log into each individual machine and opening up the log files found in /var/log.
 
-run docker image:
-docker run -p 3000:3000 cribl-api
+# Design
+
+Provide design documentation.
+
+The crux of the problem, IMO, is to read the stream using small block sizes.
+By default, I use 64KiB.
+Files are read block by block from the end.
+That way, we save a TON of time, skipping blocks at the top.
+
+# Usage
+
+Provide usage instructions.
+
+build docker image: docker build -t cribl-api .
+
+run docker image: docker run -p 3000:3000 cribl-api
+
+# Testing
+
+Provide testing instructions.
--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const url = require('url');
 
-
 /**
  * App Configuration
  */
 dotenv.config();
 
-const app = express();
+if (!process.env.PORT) {
+    process.exit(1);
+}
 const port = process.env.PORT;
 
+const app = express();
 
 /**
  * App Start
@@ -23,12 +25,22 @@ app.listen(port, () => {
     console.log(`Running crible API at http://localhost:${port}`);
 });
 
+/**
+ * GET lines
+ * 
+ * The endpoint returns a specified numer of lines from a givel log file.
+ * Returned linest are order by event's date/time (newest first).
+ * 
+ * Supported query parameters:
+ *  a. filename (within /var/log)
+ *  b. filter results based on basic text/keyword matches
+ *  c. specify the last n number of matching entries to retrieve within the log
+ */
 app.get('/', (req, res) => {
     const queryObject = url.parse(req.url, true).query;
     console.log(JSON.stringify(queryObject));
     res.send('Received API GET request.');
 });
-
 
 /***
  * App Termination
